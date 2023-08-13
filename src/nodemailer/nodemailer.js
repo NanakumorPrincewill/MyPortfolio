9c9511a229b6a6bd8e9b7cd8c497e45935fb0300
Kuragaviria <104266475+Kuragaviria@users.noreply.github.com>
2023-08-13 20:36:30 +0300

    updated

--- src/nodemailer/nodemailer.js
+++ src/nodemailer/nodemailer.js
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL;
+const emailZ = process.env.EMAILZ;
 const pass = process.env.EMAIL_PASSWORD;
 
 export const transporter = nodemailer.createTransport({
@@ -13,5 +14,5 @@ export const transporter = nodemailer.createTransport({
 
 export const mailOptions = {
   from: email,
-  to: email,
+  to: emailZ,
 };
